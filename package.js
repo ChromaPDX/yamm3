52acc2a7fb855ce36267ba38c1b3a4a9b68bc520
adam wong <adamwong246@gmail.com>
2015-06-25 15:04:29 -0700

    init meteor support

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'yamm3',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('yamm3.js');
+  api.addFiles([
+    'yamm/yamm.css',
+  ], 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('yamm3');
+  api.addFiles('yamm3-tests.js');
+});
